114a52bd6bb7ee81afb659827d11c00637359ebb
akTARDIGRADE13 <itmtha@gmail.com>
2026-01-14 23:03:21 +0900

    feat: 例の追加

---
+++ example/example_yukicoder.test.cpp
@@ -0,0 +1,10 @@
+#define PROBLEM "https://yukicoder.me/problems/no/9000"
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  string S;
+  cin >> S;
+  cout << "Hello World!" << endl;
+  return 0;
+}
